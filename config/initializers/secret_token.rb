3c4afb3ca0cc531e10678cd78718c7b6fae7bee5
David K. Pham <davidkpham@Davids-MacBook-Air.local>
2012-07-10 02:08:33 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radar::Application.config.secret_token = 'aadbd16929157ff746acf17d1bd49c51fce288ed4a8874200c70020baa2e4c3aef36f6f8049e4bb449f751e11b36fca6e784b32046f0ba6107da40dbe0bd5fd4'
