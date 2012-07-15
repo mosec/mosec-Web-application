9037c1387e288d433921a3b85005ac0595719215
David K. Pham <davidkpham@Davids-MacBook-Air.local>
2012-07-14 21:46:52 -0400

    Display global activity, removed conditional to see if results exist when searching globally since it will always be true, and no longer deleting id from params in ContactsController#show action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,6 @@ class ContactsController < ApplicationController
 		@contact = current_user.phones.first.contacts.find(params[:id])
 
 		params[:contact_id] = params[:id]
-		params.delete(:id)
 
 		@activities = Search.search(current_user.id, params).results
 	end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,6 +5,8 @@ class DashboardsController < ApplicationController
   	# TODO: user.phones.first.contacts
   	if current_user.phones.first
 	  	@contacts = current_user.phones.first.contacts.alphabetical
+	  	
+	  	@activities = Search.search(current_user.id, params).results
 	  end
   end
 end
