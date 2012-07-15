955643c49fabfd126ab9ce7f5374f5225fe99ff3
David K. Pham <davidkpham@Davids-MacBook-Air.local>
2012-07-14 21:43:54 -0400

    Now displaying full contact activities

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,10 @@ class ContactsController < ApplicationController
 	def show
 		# TODO: user.phones.first.contacts
 		@contact = current_user.phones.first.contacts.find(params[:id])
+
+		params[:contact_id] = params[:id]
+		params.delete(:id)
+
+		@activities = Search.search(current_user.id, params).results
 	end
 end
